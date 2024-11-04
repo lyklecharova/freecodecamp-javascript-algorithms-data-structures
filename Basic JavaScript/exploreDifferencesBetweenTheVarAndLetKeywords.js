75447ac2bd387be71b03ac4666ad84f8919f1e52
lyklecharova <lklecharova9@gmail.com>
2024-11-04 18:55:44 +0200

    Create exploreDifferencesBetweenTheVarAndLetKeywords.js

---
+++ Basic JavaScript/exploreDifferencesBetweenTheVarAndLetKeywords.js
@@ -0,0 +1,13 @@
+/*
+    A keyword called let was introduced in ES6, 
+    a major update to JavaScript, to solve this potential issue with the var keyword. 
+    You'll learn about other ES6 features in later challenges.
+*/
+
+//Update the code so it only uses the let keyword.
+/*
+    var catName = "Oliver";
+    var catSound = "Meow!";
+*/
+let catName = "Oliver";
+let catSound = "Meow!";
