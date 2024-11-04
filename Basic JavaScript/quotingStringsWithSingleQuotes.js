b13df89b844d9dfcc58cfa0a1f9a064bc533b88d
lyklecharova <lklecharova9@gmail.com>
2024-11-04 21:13:04 +0200

    Create quotingStringsWithSingleQuotes.js

---
+++ Basic JavaScript/quotingStringsWithSingleQuotes.js
@@ -0,0 +1,10 @@
+/*
+    String values in JavaScript may be written with single or double quotes, 
+    as long as you start and end with the same type of quote. 
+    Unlike some other programming languages, 
+    single and double quotes work the same in JavaScript.
+*/
+
+// const myStr = "<a href=\"http://www.example.com\" target=\"_blank\">Link</a>";
+
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
