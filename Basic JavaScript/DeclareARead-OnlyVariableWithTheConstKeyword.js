3259b1a01c5f8e93ab5c81208578e0f05b0a876e
lyklecharova <lklecharova9@gmail.com>
2024-11-04 18:55:15 +0200

    Create DeclareARead-OnlyVariableWithTheConstKeyword.js

---
+++ Basic JavaScript/DeclareARead-OnlyVariableWithTheConstKeyword.js
@@ -0,0 +1,20 @@
+/*
+    The keyword let is not the only new way to declare variables. 
+    In ES6, you can also declare variables using the const keyword
+
+    const has all the awesome features that let has, 
+    with the added bonus that variables declared using const are read-only. 
+    They are a constant value, which means that once a variable is assigned with const
+*/
+
+/*
+    var fCC = "freeCodeCamp"; // Change this line
+    var fact = "is cool!"; // Change this line
+    fact = "is awesome!";
+    console.log(fCC, fact); // Change this line
+*/
+
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
