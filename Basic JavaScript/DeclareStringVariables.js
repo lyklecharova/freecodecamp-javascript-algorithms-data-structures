034954e9e4b61159479d800712e6a1c56379e02d
lyklecharova <lklecharova9@gmail.com>
2024-11-04 18:56:34 +0200

    Create DeclareStringVariables.js

---
+++ Basic JavaScript/DeclareStringVariables.js
@@ -0,0 +1,7 @@
+/*
+    Create two new string variables: 
+    myFirstName and myLastName and assign 
+    them the values of your first and last name, respectively.
+*/
+var myFirstName = 'first name';
+var myLastName = 'last name';
