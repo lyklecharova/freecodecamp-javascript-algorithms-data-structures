47990170bc6ed03f4de896fe04e4479fecb2315d
lyklecharova <lklecharova9@gmail.com>
2024-11-04 21:12:57 +0200

    add

---
+++ Basic JavaScript/escapingLiteralQuotesInStrings.js
@@ -0,0 +1,13 @@
+/*
+    In JavaScript, you can escape a quote from considering 
+    it as an end of string quote by placing a backslash (\) 
+    in front of the quote.
+*/
+
+/*
+    Use backslashes to assign a string to the myStr variable so that
+    if you were to print it to the console, you would see:
+ */
+
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\".";
---
+++ Basic JavaScript/findingARemainder.js
@@ -0,0 +1,11 @@
+// The remainder operator % gives the remainder of the division of two numbers.
+/*
+    In mathematics, a number can be checked to be even or odd
+    by checking the remainder of the division of the number by 2. 
+    Even numbers have a remainder of 0, while odd numbers a remainder of 1.
+*/
+
+// Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
+
+const remainder = 11 % 3;
+console.log(remainder);
