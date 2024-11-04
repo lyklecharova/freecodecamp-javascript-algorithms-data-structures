7cc3b0a14ca333433b4fb817656e477a2a843747
lyklecharova <lklecharova9@gmail.com>
2024-11-04 21:12:35 +0200

    add

---
+++ Basic JavaScript/compoundAssignmentWithAugmentedAddition.js
@@ -0,0 +1,16 @@
+//Convert the assignments for a, b, and c to use the += operator.
+
+
+// Only change code below this line
+// a = a + 12;
+// b = 9 + b;
+// c = c + 7;
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
---
+++ Basic JavaScript/compoundAssignmentWithAugmentedDivision.js
@@ -0,0 +1,14 @@
+//Convert the assignments for a, b, and c to use the /= operator.
+
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+// a = a / 12;
+// b = b / 4;
+// c = c / 11;
+
+a /= 12;
+b /= 4;
+c /= 11;
