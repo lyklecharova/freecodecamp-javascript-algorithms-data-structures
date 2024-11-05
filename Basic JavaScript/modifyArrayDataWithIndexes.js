2e13a465505c57955caf8fac635298c2f2ed4da0
lyklecharova <lklecharova9@gmail.com>
2024-11-05 06:23:49 +0200

    Create modifyArrayDataWithIndexes.js

---
+++ Basic JavaScript/modifyArrayDataWithIndexes.js
@@ -0,0 +1,12 @@
+/*
+    Unlike strings, the entries 
+    of arrays are mutable and can be changed freely, even if the array was declared with const.
+*/
+
+//Modify the data stored at index 0 of myArray to a value of 45.
+
+// Setup
+const myArray = [18, 64, 99];
+
+// Only change code below this line
+myArray[0] = 45;
