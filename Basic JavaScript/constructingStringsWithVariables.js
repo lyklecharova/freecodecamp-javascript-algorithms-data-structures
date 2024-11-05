91178091d5896abde6e2a23243e2a5f2cd48f06f
lyklecharova <lklecharova9@gmail.com>
2024-11-05 08:46:34 +0200

    Create constructingStringsWithVariables.js

---
+++ Basic JavaScript/constructingStringsWithVariables.js
@@ -0,0 +1,16 @@
+/*
+    Sometimes you will need to build a string. 
+    By using the concatenation operator (+), 
+    you can insert one or more variables into a string you're building.
+*/
+
+/*
+    Set myName to a string equal to 
+    your name and build myStr with myName 
+    between the strings My name is  and  and I am well!
+*/
+
+
+// Only change code below this line
+const myName = "Hello, JS";
+const myStr = " and I am well! " + myName + " , nice too meet you!";
