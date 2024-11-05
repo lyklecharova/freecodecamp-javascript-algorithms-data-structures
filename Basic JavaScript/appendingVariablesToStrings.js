aab6328f90a6bc9ac5cc964b1c437efc42fcceaa
lyklecharova <lklecharova9@gmail.com>
2024-11-05 17:27:56 +0200

    Create appendingVariablesToStrings.js

---
+++ Basic JavaScript/appendingVariablesToStrings.js
@@ -0,0 +1,5 @@
+// Set someAdjective to a string of at least 3 characters and append it to myStr using the += operator.
+
+const someAdjective = "JS !";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
