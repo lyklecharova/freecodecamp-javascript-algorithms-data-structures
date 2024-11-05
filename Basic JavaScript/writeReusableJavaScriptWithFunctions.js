aaaf21c74d33a8684ce9d1032caf5f70ecf09720
lyklecharova <lklecharova9@gmail.com>
2024-11-05 22:55:16 +0200

    Create writeReusableJavaScriptWithFunctions.js

---
+++ Basic JavaScript/writeReusableJavaScriptWithFunctions.js
@@ -0,0 +1,13 @@
+/*
+    In JavaScript, we can divide up our code into reusable parts called functions.
+*/
+
+/*
+    Create a function called reusableFunction which prints the string Hi World to the dev console.
+    Call the function.
+*/
+
+function reusableFunction(){
+    console.log('Hi World');
+}
+reusableFunction();
