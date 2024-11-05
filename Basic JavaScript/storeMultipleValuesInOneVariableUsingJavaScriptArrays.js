ba4c570f4a57fabf9241bd1eacd12afa5ab885ee
lyklecharova <lklecharova9@gmail.com>
2024-11-05 06:23:42 +0200

    Create storeMultipleValuesInOneVariableUsingJavaScriptArrays.js

---
+++ Basic JavaScript/storeMultipleValuesInOneVariableUsingJavaScriptArrays.js
@@ -0,0 +1,5 @@
+//With JavaScript array variables, we can store several pieces of data in one place.
+
+//With JavaScript array variables, we can store several pieces of data in one place.
+
+const myArray = ["code", 0];
