a01ceaba21aeefde4e367eba1030a93b4e58f738
lyklecharova <lklecharova9@gmail.com>
2024-11-06 06:36:08 +0200

    Create returnAValueFromAFunctionWithReturn.js

---
+++ Basic JavaScript/returnAValueFromAFunctionWithReturn.js
@@ -0,0 +1,14 @@
+/*
+    We can pass values into a function with arguments. 
+    You can use a return statement to send a value back out of a function.
+*/
+
+// Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+function timesFive(number){
+    return number * 5;
+}
+console.log(timesFive(5)); //25
+console.log(timesFive(2)); //10
+console.log(timesFive(0)); //0
+
