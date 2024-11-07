c876b2175a157bacb39fdf5e162b924f0bcb8fdc
lyklecharova <lklecharova9@gmail.com>
2024-11-07 07:35:09 +0200

    Create comparisonWithTheStrictEqualityOperator.js

---
+++ Basic JavaScript/comparisonWithTheStrictEqualityOperator.js
@@ -0,0 +1,21 @@
+/*
+    Strict equality (===) is the counterpart to the equality operator (==). 
+    However, unlike the equality operator, which attempts to convert both values 
+    being compared to a common type, the strict equality operator does not perform a type conversion.
+*/
+
+/*
+    Use the strict equality operator in the if statement so the function 
+    will return the string Equal when val is strictly equal to 7.
+*/
+
+// Setup
+function testStrict(val) {
+    if (val === 7) { // Change this line
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+console.log(testStrict('7'));
+console.log(testStrict(7));
