d911c5a1410589bf3eebbcacf69148205849118c
lyklecharova <lklecharova9@gmail.com>
2024-11-13 06:35:33 +0200

    Create buildJavaScriptObjects.js

---
+++ Basic JavaScript/buildJavaScriptObjects.js
@@ -0,0 +1,10 @@
+const myDog = {
+   "name": 'Sharo',
+   "legs": 4,
+   "tails": 1,
+   "friends": ['Marko', 'Rex', 'Rubby']
+};
+console.log(myDog.name);
+console.log(myDog.legs);
+console.log(myDog.tails);
+console.log(myDog.friends);
