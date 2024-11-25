c2cd42fba69b09a634d4358620aeb098951cd4d5
lyklecharova <lklecharova9@gmail.com>
2024-11-25 07:03:52 +0200

    Create manipulatingComplexObjects.js

---
+++ Basic JavaScript/manipulatingComplexObjects.js
@@ -0,0 +1,19 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "Adele", 
+        "title": "25",
+        "release_year": 2015,
+        "formats": ["CD", "Digital", "Vinyl"]
+    }
+];
