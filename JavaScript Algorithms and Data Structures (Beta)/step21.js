9ee1d4f48d07249bd7bd7ddc44cc1b9bd6f7c991
Lilyana Klecharova <lklecharova9@gmail.com>
2024-12-10 08:58:19 +0200

    add

---
+++ JavaScript Algorithms and Data Structures (Beta)/step21.js
@@ -0,0 +1,12 @@
+/*
+    Update the third element of your rows array to be the number 10. 
+    Then print the rows array to your console.
+*/
+
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+
+rows[2] = 10; // Update the third element to 10
+console.log(rows); 
